a37b76fea0f2474efc873b7c8ae52906e2a77991
darshnablah <dwright758@gmail.com>
2021-06-03 14:58:51 -0500

    add comments to code

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// get necessary elements
 const popupContainer = document.querySelector('.popup-container');
 const openBtn = document.querySelector('#open-btn');
 
-
+// add click listener to open/close button
 openBtn.addEventListener('click', () => {
+  // unhide the form
   popupContainer.classList.toggle('shown');
+  // change button style and content depending on the state of the form
   if (popupContainer.classList.contains('shown')) {
     openBtn.classList.add('red');
     openBtn.textContent = 'Close Form';
